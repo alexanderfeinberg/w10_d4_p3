635d41bae1d62c6c2f5336f498410053d5aa87fd
alexander1fein <feinberg.alexander@gmail.com>
2022-09-09 12:42:14 -0400

    Done

--- sql-practice/music.sql
+++ sql-practice/music.sql
@@ -11,7 +11,7 @@ CREATE TABLE musicians (
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100),
   band_id INTEGER,
-  FOREIGN KEY (band_id) REFERENCES bands(id)
+  FOREIGN KEY (band_id) REFERENCES bands(id) ON DELETE CASCADE
 );
 INSERT INTO bands
 VALUES (1, 'The Falling Box'),
@@ -30,3 +30,6 @@ VALUES (1, 'Adam', 'Appleby', 1),
   (8, 'Camila', 'Nenci', 4),
   (9, 'Rosemarie', 'Affini', 5),
   (10, 'Victoria', 'Cremonesi', 5);
+
+
+  DELETE FROM bands WHERE id=1;
